e5bf3d2665ae56b90efcca5d5e681420b0335d1f
npierre9 <nicolas.pierre091@gmail.com>
2021-01-12 15:55:06 +0100

    Initial commit

---
+++ Faircorp/app/src/main/java/com/example/faircorp/WindowActivity.kt
@@ -0,0 +1,17 @@
+package com.example.faircorp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class WindowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_window)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val param = intent.getStringExtra(WINDOW_NAME_PARAM)
+        val windowName = findViewById<TextView>(R.id.txt_window_name)
+        windowName.text = param
+    }
+}
