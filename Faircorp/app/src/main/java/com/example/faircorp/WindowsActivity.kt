586ff32a15866fd739cdce2bfd6a52b2806add4b
npierre9 <nicolas.pierre091@gmail.com>
2021-01-12 19:21:37 +0100

    Update Android project

--- Faircorp/app/src/main/java/com/example/faircorp/WindowsActivity.kt
+++ Faircorp/app/src/main/java/com/example/faircorp/WindowsActivity.kt
@@ -2,10 +2,27 @@ package com.example.faircorp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.faircorp.model.WindowAdapter
+import com.example.faircorp.model.WindowService
 
 class WindowsActivity : BasicActivity() {
+
+    val windowService = WindowService()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_windows)
+        val recyclerView = findViewById<RecyclerView>(R.id.list_windows) // (2)
+        val adapter = WindowAdapter() // (3)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        adapter.update(windowService.findAll()) // (4)
     }
 }
--- Faircorp/app/src/main/java/com/example/faircorp/model/WindowsAdapterView.kt
+++ Faircorp/app/src/main/java/com/example/faircorp/model/WindowsAdapterView.kt
@@ -1,4 +1,42 @@
 package com.example.faircorp.model
 
-class WindowsAdapterView {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.faircorp.R
+
+class WindowAdapter : RecyclerView.Adapter<WindowAdapter.WindowViewHolder>() { // (1)
+
+    inner class WindowViewHolder(view: View) : RecyclerView.ViewHolder(view) { // (2)
+        val name: TextView = view.findViewById(R.id.txt_window_name)
+        val room: TextView = view.findViewById(R.id.txt_window_room)
+        val status: TextView = view.findViewById(R.id.txt_status)
+    }
+
+    private val items = mutableListOf<WindowDto>() // (3)
+
+    fun update(windows: List<WindowDto>) {  // (4)
+        items.clear()
+        items.addAll(windows)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = items.size // (5)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WindowViewHolder { // (6)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_windows_item, parent, false)
+        return WindowViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: WindowViewHolder, position: Int) {  // (7)
+        val window = items[position]
+        holder.apply {
+            name.text = window.name
+            status.text = window.status.toString()
+            room.text = window.room.name
+        }
+    }
 }
